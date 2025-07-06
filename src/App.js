64d79a8054f5d348c8de7c881bd8294fd03ab559
Zack Honey <zhoney466@gmail.com>
2025-07-05 17:46:09 -0700

    initial commit and started over

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <body className='body'>
+
+    <div className='nav-container'>
+          <nav className="nav">
+            <button className="button">
+              Login
+            </button>
+            <button className="button">
+              Servers
+            </button>
+            <button className="button">
+              Projects
+            </button>
+            <button className="button">
+              Videos
+            </button>
+            <button className="button">
+              Gallery
+            </button>
+            <button className="button">
+              Monetization
+            </button>
+            <button className="button">
+              Mods List
+            </button>
+            <button className='button'>
+              My Stats
+            </button>
+          </nav>
+        </div>
+
+  </body>
+  )
 }
+
 
 export default App;
