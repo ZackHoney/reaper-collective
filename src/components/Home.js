58c4aca93156250ce8b8d1280bd4bca79d68502c
Zack Honey <zhoney466@gmail.com>
2025-07-06 20:41:27 -0700

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className="home-body">
       <h1>Home</h1>
+      <img src="/src/images/BadCoLogo_Discord_Banner2222.png" alt="Banner" className='home-body-img'/>
     </div>
   )
 }
